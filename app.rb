3156f1d8c106818c000ece6755813a9a81bace45
Kellye Greene <kellye@gust.com>
2016-06-28 10:11:47 -0400

    Sinatra routs

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Kellye"
+  end
+  
+  get "/hometown" do
+    "My hometown is Nashville"
+  end
+  
+  get "/favorite-song" do
+    "My favorite song is Kiss by Prince"
+  end
 end
